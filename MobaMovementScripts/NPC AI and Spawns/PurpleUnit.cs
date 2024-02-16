3f9284cb1d39620806792f2b7887ba3e853875d4
DrStrangeGl0ve <159051382+DrStrangeGl0ve@users.noreply.github.com>
2024-02-16 13:42:31 -0600

    Update PurpleUnit.cs

--- MobaMovementScripts/NPC AI and Spawns/PurpleUnit.cs
+++ MobaMovementScripts/NPC AI and Spawns/PurpleUnit.cs
@@ -8,16 +8,9 @@ public class PurpleUnit : ScriptableObject
     //Need to create system for counting minions of each color before spawning the waves. Need to properly name them as well.
     public GameObject newPurpleUnit;
     public string purpleUnitName;
-    //public string teamColor;
 
     public Vector3[] purpleMinionSpawnLocation = { new Vector3(-44, 0.5f, -35), new Vector3(-40, 0.5f, -40), new Vector3(-35, .5f, -45) };
     public string unitType;
     public int waveSize = 3;
-    /*public int health;
-    public int attackDamage;
-    public int magicDamage;
-    public int attackDefense;
-    public int magicResist;
-    
-*/
+ 
 }
